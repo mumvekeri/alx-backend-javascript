4ea22cfeca9ad965b8ec5121057cb271ee08c28e
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-04-23 17:08:35 +0200

    Update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,18 @@
 function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-        if (success) {
-            resolve({ status: 200, body: 'Success' });
-        } else {
-            reject(new Error('The fake API is not working currently'));
-        }
-    });
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
+
+// Testing the function
+getFullResponseFromAPI(true)
+  .then(response => console.log('Resolved:', response))
+  .catch(error => console.error('Rejected:', error.message));
+
+getFullResponseFromAPI(false)
+  .then(response => console.log('Resolved:', response))
+  .catch(error => console.error('Rejected:', error.message));
