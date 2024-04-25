debb6b29d93d1c5e66f3841af6ff787e2af0fa62
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-04-25 22:13:59 +0200

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,10 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const try = [
+  const arr = [
     new ClassRoom(19),
     new ClassRoom(20),
     new ClassRoom(34),
   ];
-  return (try);
+  return (arr);
 }
