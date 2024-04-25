771a3b77b5603877274780a72daa9ac7437644bb
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-04-25 21:07:32 +0200

    Update 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -10,9 +10,10 @@ export default class HolbertonCourse {
   }
 
   set name(newName) {
-    if (typeof newName === 'string') {
-      this._name = newName;
+    if (typeof newName !== 'string') {
+      throw new Error('Name must be a string.');
     }
+    this._name = newName;
   }
 
   get length() {
@@ -20,9 +21,10 @@ export default class HolbertonCourse {
   }
 
   set length(newLength) {
-    if (typeof newLength === 'number') {
-      this._length = newLength;
+    if (typeof newLength !== 'number') {
+      throw new Error('Length must be a number.');
     }
+    this._length = newLength;
   }
 
   get students() {
@@ -30,8 +32,9 @@ export default class HolbertonCourse {
   }
 
   set students(newStudents) {
-    if (Array.isArray(newStudents) && newStudents.every(item => typeof item === 'string')) {
-      this._students = newStudents;
+    if (!Array.isArray(newStudents)) {
+      throw new Error('Students must be an array.');
     }
+    this._students = newStudents;
   }
 }
