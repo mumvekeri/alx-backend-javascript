9e133f220ef74fb09b3541fad400fb9a48c89ad8
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-04-25 20:43:20 +0200

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,12 +1,10 @@
-// script.js
+// initializeRooms.js
 import ClassRoom from './0-classroom.js';
 
 function initializeRooms() {
-  const sizes = [19, 20, 34];
-  const rooms = sizes.map(size => new ClassRoom(size));
+  const roomSizes = [19, 20, 34];
+  const rooms = roomSizes.map(size => new ClassRoom(size));
   return rooms;
 }
 
-const rooms = initializeRooms();
-console.log(rooms);
-
+export default initializeRooms;
