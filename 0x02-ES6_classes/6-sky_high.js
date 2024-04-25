8ab881e621ba1afa802c3f8350a8ae4a05f1ba57
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-04-25 21:45:40 +0200

    Update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,32 +1,16 @@
-from abc import ABC, abstractmethod
+import Building from './5-building';
 
-class Building(ABC):
-    def __init__(self, sqft):
-        self._sqft = sqft
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-    @property
-    def sqft(self):
-        return self._sqft
+  get floors() {
+    return this._floors;
+  }
 
-    @abstractmethod
-    def evacuationWarningMessage(self):
-        pass
-
-# Example usage:
-class OfficeBuilding(Building):
-    def __init__(self, sqft, num_floors):
-        super().__init__(sqft)
-        self._num_floors = num_floors
-
-    @property
-    def num_floors(self):
-        return self._num_floors
-
-    def evacuationWarningMessage(self):
-        return "Please proceed to the nearest exit in an orderly manner."
-
-# Testing
-office = OfficeBuilding(10000, 5)
-print(office.sqft)  # Output: 10000
-print(office.num_floors)  # Output: 5
-print(office.evacuationWarningMessage())
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
