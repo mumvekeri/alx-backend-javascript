7666d5c6dfa7ab73e17a0e52f44d85c31f705ca3
mumvekeri <mumvekerichidochashe@gmail.com>
2024-04-29 11:17:20 -0700

    0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,14 @@
+const getListStudents = () => {
+    const students = [
+        { id: 1, firstName: "Guillaume", location: "San Francisco" },
+        { id: 2, firstName: "James", location: "Columbia" },
+        { id: 5, firstName: "Serena", location: "San Francisco" }
+    ];
+
+    return students;
+};
+
+// Example usage:
+const studentList = getListStudents();
+console.log(studentList);
+
