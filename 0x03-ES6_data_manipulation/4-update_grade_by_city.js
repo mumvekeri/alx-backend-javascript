9218ae7141ffe4c31654707e8f39c3b2ecf98b66
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-04-29 21:22:47 +0200

    Create 4-update_grade_by_city.js

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+const updateStudentGradeByCity = (students, city, newGrades) => {
+    return students
+        .filter(student => student.location === city)
+        .map(student => {
+            const matchingGrade = newGrades.find(grade => grade.studentId === student.id);
+            const gradeValue = matchingGrade ? matchingGrade.grade : 'N/A';
+            return { ...student, grade: gradeValue };
+        });
+};
+
+export default updateStudentGradeByCity;
