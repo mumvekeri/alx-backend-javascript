126f3e072cd71b24664a7e7c2e1c47fd7877cb2e
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-04-29 21:40:35 +0200

    Update 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,13 +1,9 @@
-const createInt8TypedArray = (length, position, value) => {
-    if (position < 0 || position >= length) {
-        throw new Error('Position outside range');
-    }
-
-    const buffer = new ArrayBuffer(length);
-    const view = new Int8Array(buffer);
-    view[position] = value;
-
-    return buffer;
-};
-
-export default createInt8TypedArray;
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw Error('Position outside range');
+  }
+  const newBuffer = new ArrayBuffer(length);
+  const int8 = new Int8Array(newBuffer, 0, length);
+  int8.set([value], position);
+  return new DataView(newBuffer);
+}
