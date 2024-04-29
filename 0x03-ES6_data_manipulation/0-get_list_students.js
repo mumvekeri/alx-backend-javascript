0dff94c5dd998db960cd6e6acab76363d21c3ed3
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-04-29 20:23:05 +0200

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,14 +1,7 @@
-const getListStudents = () => {
-    const students = [
-        { id: 1, firstName: "Guillaume", location: "San Francisco" },
-        { id: 2, firstName: "James", location: "Columbia" },
-        { id: 5, firstName: "Serena", location: "San Francisco" }
-    ];
+const getListStudents = () => [
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' },
+];
 
-    return students;
-};
-
-// Example usage:
-const studentList = getListStudents();
-console.log(studentList);
-
+export default getListStudents;
