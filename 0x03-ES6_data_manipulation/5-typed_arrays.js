5fbe63416cacfad6192fc1d72c9248ea8e188efb
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-04-29 21:39:09 +0200

    Create 5-typed_arrays.js
    const createInt8TypedArray = (length, position, value) => {
        if (position < 0 || position >= length) {
            throw new Error('Position outside range');
        }
    
        const buffer = new ArrayBuffer(length);
        const view = new Int8Array(buffer);
        view[position] = value;
    
        return buffer;
    };
    
    export default createInt8TypedArray;

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,13 @@
+const createInt8TypedArray = (length, position, value) => {
+    if (position < 0 || position >= length) {
+        throw new Error('Position outside range');
+    }
+
+    const buffer = new ArrayBuffer(length);
+    const view = new Int8Array(buffer);
+    view[position] = value;
+
+    return buffer;
+};
+
+export default createInt8TypedArray;
