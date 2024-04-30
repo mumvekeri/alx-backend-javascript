e06a8a124bbf7209185b989b4567e82c637223cc
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-04-30 21:14:47 +0200

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,8 @@
 const getListStudentIds = (myArray) => {
-    if (!Array.isArray(myArray)) {
-        return [];
-    }
-    return myArray.map((studentId) => studentId.id);
+  if (!Array.isArray(myArray)) {
+    return [];
+  }
+  return myArray.map((studentId) => studentId.id);
 };
 
 export default getListStudentIds;
-
