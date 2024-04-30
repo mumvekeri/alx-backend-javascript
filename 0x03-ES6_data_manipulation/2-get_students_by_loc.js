9bd05640230d1b6c74e26860b4163c477f4beb1a
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-04-30 22:23:31 +0200

    Update 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,5 @@
 const getStudentsByLocation = (students, city) => {
-    return students.filter(student => student.location === city);
+    return students.filter((student) => student.location === city);
 };
 
-export default getStudentsByLocation; 
+export default getStudentsByLocation;
